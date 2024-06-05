0d107e57566546bbabd9d58fc703ccb35a0fb8da
Eugene <git@telegrafo.ru>
2024-06-05 21:32:22 +0500

    Improve tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,4 @@
 Mimic.copy(UrFUAPI.UBU.Client)
+Mimic.copy(UrFUAPI.IStudent.Client)
+
 ExUnit.start(exclude: :integration, capture_log: true)
--- test/urfu_api/istudent/auth_test.exs
+++ test/urfu_api/istudent/auth_test.exs
@@ -1,23 +1,59 @@
 defmodule UrFUAPI.IStudent.AuthTest do
   use ExUnit.Case, async: true
+  use Mimic.DSL
 
   alias UrFUAPI.IStudent.Auth
+  alias UrFUAPI.IStudent.Auth.Token
+  alias UrFUAPI.IStudent.Client
 
-  setup context do
-    if context[:integration] do
+  describe "Real API" do
+    @describetag :integration
+
+    setup do
       UrFUAPI.Credentials.fetch_from_env()
-    else
-      :ok
+    end
+
+    test "auths with valid credentials", %{username: username, password: password} do
+      assert {:ok, _auth} = Auth.sign_in(username, password)
+    end
+
+    test "not auths with invalid credentials" do
+      assert {:error, _reason} = Auth.sign_in("username", "password")
     end
   end
 
-  @tag :integration
-  test "Auth with valid credentials", %{username: username, password: password} do
-    assert {:ok, _auth} = Auth.sign_in(username, password)
-  end
+  describe "sign_in/2" do
+    test "returns error on tesla error" do
+      expect(Client.request_access_token(_username, _password), do: {:error, :tesla_error})
 
-  @tag :integration
-  test "Auth with invalid credentials" do
-    assert {:error, _auth} = Auth.sign_in("username", "password")
+      assert {:error, :tesla_error} = Auth.sign_in("username", "password")
+    end
+
+    test "returns error on invalid_grant" do
+      expect(Client.request_access_token(_username, _password),
+        do: {:ok, %{"error" => "invalid_grant"}}
+      )
+
+      assert {:error, :wrong_credentials} = Auth.sign_in("username", "password")
+    end
+
+    test "returns error on success without access_token" do
+      expect(Client.request_access_token(_username, _password),
+        do: {:ok, %{}}
+      )
+
+      assert {:error, :invalid_server_response} = Auth.sign_in("username", "password")
+    end
+
+    test "returns ok on success" do
+      username = "some-username"
+      access_token = "good-token"
+
+      expect(Client.request_access_token(_username, _password),
+        do: {:ok, %{"access_token" => access_token}}
+      )
+
+      assert {:ok, %Token{access_token: access_token, username: username}} == Auth.sign_in(username, "password")
+    end
   end
 end
